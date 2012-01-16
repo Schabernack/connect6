acf867c98a6fce5b7fc22d4c7709df13b1308a18
nico <mister.wunderlich@gmail.com>
2012-01-16 21:35:08 +0100

    get folgesituationen

--- c6.py
+++ c6.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import itertools
 from gameboard import GameBoard 
 
 class MinMax:
@@ -19,13 +20,28 @@ class MinMax:
 		game = GameBoard()
 		graph = nx.DiGraph()
 		gamelist = []
+		coordinates = []
 		for row in range(18):
 			for col in range(17):
 				board = gamesituation.get_copy()
 				if  not board.get_board()[row][col] and not board.get_board()[row][col+1]:
 					board.get_board()[row][col]=color
-					board.get_board()[row][col+1]=color
-					gamelist.append(board)
+		for row in range(18):
+			for col in range(18):
+				coordinates.append((row,col))
+		
+		coordinate_tuple = it.product(coordinates, coordinates)
+		for ctuple in coordinate_tuple:
+			if  not board.get_board()[ctuple[0][0]][ctuple[0][1]] and not board.get_board()[ctuple[1][0]][ctuple[1][1]]:		
+				board = gamesituation.get_copy()
+				if color=='B':
+					board.put_black(ctuple[0][0],[ctuple[0][1])
+					board.put_black(ctuple[1][0],[ctuple[1][1])
+				else:
+					board.put_white(ctuple[0][0],[ctuple[0][1])
+					board.put_white(ctuple[1][0],[ctuple[1][1])
+			gamelist.append(board)
+			
 		return gamelist
 		
 	def build_game_graph(self, board, depth):
@@ -42,6 +58,7 @@ class MinMax:
 
 	if __name__ == "__main__":
 		game = GameBoard()
+		get_gametree_children(game, 'B')
 		game.put_black(3,3)
 		game.put_black(3,4)
 		game.put_black(3,5)
