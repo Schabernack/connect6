f02c1a1b925f7aa4ab8c9c64cec8293c8d152d16
Denniz <d_zim@gmx.de>
2012-01-19 20:53:12 +0100

    working solution ... maybe

--- c6.py
+++ c6.py
@@ -2,41 +2,28 @@ import itertools
 import datetime
 from gameboard import GameBoard 
 from coord import Coord
+from referee import Referee
+from player import Player
+from dummyai import DummyAI
 
 
+class Game:
 
-class Player:
+	
+	def run(self):
+		ref = Referee()
+		p1 = DummyAI()
+		p2 = Player()
 
-	##Todo: Implement alpha-beta pruning
-	def minimax(node, depth):
-		if depth <= 0:
-			return asses(node)
-		alpha = -Infinity
-		for c in node:
-			alpha = max(alpha, -minimax(child, depth-1))
-		
-		return alpha
-
-
-	def span_tree_for_node(self,board,depth):
-		node = Node(board)
-		
-		if depth <= 0:
-			return node
-
-		for b in board.get_next_moves():
-			node.add_child(span_tree_for_node(b,depth-1))
+		ref.add_player(p1)
+		ref.add_player(p2)
 
 		
 
-	if __name__ == "__main__":
+if __name__ == "__main__":
 		
-		game = GameBoard()
-		game.put_token(Coord(9,9),'B')
-		game.put_token(Coord(9,14),'B')
-		game.put_token(Coord(9,10),'B')
-		
-		game.drucken()
+	g = Game()
+	g.run()
 
 		
 
--- player.py
+++ player.py
@@ -3,7 +3,6 @@ import datetime
 from gameboard import GameBoard 
 from coord import Coord
 from sets import Set
-
 # for further explanation of tactic used, see
 # Wu, Zhou: Optimization of the Connect6 Classical Evaluation Function
 # Based on Threat Theory and Game Strategy (2010)
@@ -25,7 +24,6 @@ class Player:
 	def_rows = []
 
 	
-
 	def __init__(self):
 		self.init_boards()
 
@@ -38,6 +36,7 @@ class Player:
 		if len(message) == 1:
 			self.color = 'D'
 			self.enemy = 'L'
+			return "1010"
 		elif len(message) == 5:
 			self.color = 'L'
 			self.enemy = 'D'
@@ -48,11 +47,18 @@ class Player:
 			coord2 = Coord(int(message[4:6]),int(message[6:8]))
 			self.put_enemy_stones(coord1,coord2)
 
-		self.board.put_token(Coord(1,1),self.color)
+		return self.get_message(self.do_best_move())
 
-	
+	def get_message(self,coord1,coord2):
+		msg = ""
 
+		msg += coord1.get_0_prefixed_repr()
+		msg += coord2.get_0_prefixed_repr()
 
+		print msg
+
+		return msg
+		
 
 	# live 5: just need to put one stone to form a Conn-6,
 	# but the opponent need to put two stones to prevent it
@@ -81,65 +87,273 @@ class Player:
 	
 	#returns 2 Coords
 	def do_best_move(self):
+		self.get_rows()
 		# if any of our sleep/alive 4/5 exists, make a conn6
-		live5 = self.get_alive_off(5)
-		if live5:
-			return self.getCoord(live5[1][0]), self.getCoord(live5[1][1]) 
+		alive5 = self.get_alive_off(5)
+		if alive5:
+			return self.getCoord(alive5[0][1][0]), self.getCoord(alive5[0][1][1]) 
 			
 		sleep5 = self.get_sleeping_off(5)
 		if sleep5:
-			self.board.put_token(sleep5[1][0], self.color)
-			return self.getCoord(sleep5[1][0]), self.get_legal_move()
+			return self.getCoord(sleep5[0][1][0]), self.get_legal_move(1)
 
-		live4 = self.get_alive_off(4)
+		alive4 = self.get_alive_off(4)
 		if live4:
-			return self.getCoord(live4[1][0]), self.getCoord(live4[1][1])
+			return self.getCoord(alive4[0][1][0]), self.getCoord(alive4[0][1][1])
+		
 		sleep4 = self.get_sleeping_off(4)
-		## toDo: Find next move!
 		if sleep4:
-			return self.getCoord(sleep4[1][0]), self.getCoord(sleep4[1][1])
+			for row in sleep4:
+				coord1 = self.getCoord(row[1][0])
+				direction = row[2]
+
+				##check if win is possible otherwise check next row
+				if (direction == VER):
+					if self.board[coord1.row-1][coord1.col]=='':
+						return coord1, Coord(coord1.row-1)(coord1.col)
+					elif self.board[coord1.row+1][coord1.col]=='':
+						return coord1, Coord(coord1.row+1)(coord1.col)
+				elif (direction == HOR):
+					if self.board[coord1.row][coord1.col-1]=='':
+						return coord1, Coord(coord1.row)(coord1.col-1)
+					elif self.board[coord1.row][coord1.col+1]=='':
+						return coord1, Coord(coord1.row)(coord1.col+1)
+				elif (direction == DRI):
+					if self.board[coord1.row-1][coord1.col-1]=='':
+						return coord1, Coord(coord1.row-1)(coord1.col-1)
+					elif self.board[coord1.row+1][coord1.col+1]=='':
+						return coord1, Coord(coord1.row+1)(coord1.col+1)
+				else:
+					if self.board[coord1.row-1][coord1.col+1]=='':
+						return coord1, Coord(coord1.row-1)(coord1.col+1)
+					elif self.board[coord1.row+1][coord1.col-1]=='':
+						return coord1, Coord(coord1.row+1)(coord1.col-1)
+
 		
 		# if enemy has sleep/alive 4/5 exists, make it dead
 		alive5 = self.get_alive_def(5)
 		if alive5:
-			return self.getCoord(alive5[1][0]), self.getCoord(alive5[1][1])
+			return self.getCoord(alive5[0][1][0]), self.getCoord(alive5[0][1][1])
+		
+		alive4 = self.get_alive_def(4)
+		if alive4:
+			return self.getCoord(sleep4[0][1][0]), self.getCoord(alive4[0][1][1])
+
+
 		sleep5 = self.get_sleeping_def(5)
 		if sleep5:
 			# If only one sleep 5? improve own standing, else: destroy other sleep5
-			return self.getCoord(sleep5[1][0]), self.get_legal_move()
-		alive4 = self.get_alive_def(4)
-		if alive4:
-			return self.getCoord(sleep4[1][0]), self.getCoord(alive4[1][1])
+			if len(sleep5) == 1:
+				return self.getCoord(sleep5[0][1][0]), self.get_improvement_move(1)
+			else:
+				return self.getCoord(sleep5[0][1][0]), self.getCoord(sleep5[1][1][0])
+		
+		## toDo only put to sleep if conn-6 is possible
+
 		sleep4 = self.get_sleeping_def(4)
 		if sleep4:
-			return self.getCoord(sleep4[1][0]), self.getCoord(alive4[1][1])
+			if len(sleep4) == 1:
+				return self.getCoord(sleep4[0][1][0]), self.get_improvement_move(1)
+			else:
+				return self.getCoord(sleep4[0][1][0]), self.getCoord(sleep4[1][1][0])
+		
+		return self.get_improvement_move(2)
+
+
+	## Avoided opponent win .. do best move
+	## Put enemy live2/3 to sleep
+	## ...
+	def get_improvement_move(self,count):
+		#
+		alive3 = self.get_alive_def(3)
+		if alive3:
+			if len(alive3) == 1: 
+				if count == 2:
+					return self.getCoord(alive3[0][1][0]), self.do_offensive_move(1)
+				else: 
+					return self.getCoord(alive3[0][1][0])
+			else:
+				if count == 2:
+					return self.getCoord(alive3[0][1][0]), self.getCoord(alive3[1][1][0])
+				else:
+					return self.getCoord(alive3[0][1][0])
+		
+		alive2 = self.get_alive_def(2)
+		if alive2:
+			if len(alive2) == 1: 
+				if count == 2:
+					return self.getCoord(alive2[0][1][0]), self.do_offensive_move(1)
+				else: 
+					return self.getCoord(alive2[0][1][0])
+			else:
+				if count == 2:
+					return self.getCoord(alive2[0][1][0]), self.getCoord(alive2[1][1][0])
+				else:
+					return self.getCoord(alive2[0][1][0])
+
+		return self.do_offensive_move(2)
+
+	## enemy live2/3 were put to sleep
+	## remaining moves for offense
+
+	def get_offensive_move(self,count):
+		returnCount = count
+		moves = []
+		while (count > 0):
+			alive3 = self.get_alive_off(3)
+
+			for row in alive3:
+				coord1 = self.getCoord(row[1][0])
+				direction = row[2]
+
+				##check if win is possible otherwise check next row
+				if (direction == VER):
+					if self.board[coord1.row-1][coord1.col]=='':
+						moves.append(coord1)
+						count -= 1
+					elif self.board[coord1.row+1][coord1.col]=='':
+						moves.append(coord1)
+						count -= 1
+				elif (direction == HOR):
+					if self.board[coord1.row][coord1.col-1]=='':
+						moves.append(coord1)
+						count -= 1
+					elif self.board[coord1.row][coord1.col+1]=='':
+						moves.append(coord1)
+						count -= 1
+				elif (direction == DRI):
+					if self.board[coord1.row-1][coord1.col-1]=='':
+						moves.append(coord1)
+						count -= 1
+					elif self.board[coord1.row+1][coord1.col+1]=='':
+						moves.append(coord1)
+						count -= 1
+				else:
+					if self.board[coord1.row-1][coord1.col+1]=='':
+						moves.append(coord1)
+						count -= 1
+					elif self.board[coord1.row+1][coord1.col-1]=='':
+						moves.append(coord1)
+						count -= 1
+			if (count > 0):	
+				alive2 = self.get_alive_off(2)
+
+				for row in alive2:
+					coord1 = self.getCoord(row[1][0])
+					direction = row[2]
+
+					##check if win is possible otherwise check next row
+					if (direction == VER):
+						if self.board[coord1.row-1][coord1.col]=='':
+							moves.append(coord1)
+							count -= 1
+						elif self.board[coord1.row+1][coord1.col]=='':
+							moves.append(coord1)
+							count -= 1
+					elif (direction == HOR):
+						if self.board[coord1.row][coord1.col-1]=='':
+							moves.append(coord1)
+							count -= 1
+						elif self.board[coord1.row][coord1.col+1]=='':
+							moves.append(coord1)
+							count -= 1
+					elif (direction == DRI):
+						if self.board[coord1.row-1][coord1.col-1]=='':
+							moves.append(coord1)
+							count -= 1
+						elif self.board[coord1.row+1][coord1.col+1]=='':
+							moves.append(coord1)
+							count -= 1
+					else:
+						if self.board[coord1.row-1][coord1.col+1]=='':
+							moves.append(coord1)
+							count -= 1
+						elif self.board[coord1.row+1][coord1.col-1]=='':
+							moves.append(coord1)
+							count -= 1
+				if (count > 0):
+					alive1 = self.get_alive_off(1)
+					for row in alive1:
+						coord1 = self.getCoord(row[1][0])
+						moves.append(coord1)
+						count -= 1
+					
+					if (count > 0):	
+						sleep1 = self.get_sleeping_off(2)
+
+						for row in sleep1:
+							coord1 = self.getCoord(row[1][0])
+							direction = row[2]
+
+							##check if win is possible otherwise check next row
+							if (direction == VER):
+								if self.board[coord1.row-1][coord1.col]=='':
+									moves.append(coord1)
+									count -= 1
+								elif self.board[coord1.row+1][coord1.col]=='':
+									moves.append(coord1)
+									count -= 1
+							elif (direction == HOR):
+								if self.board[coord1.row][coord1.col-1]=='':
+									moves.append(coord1)
+									count -= 1
+								elif self.board[coord1.row][coord1.col+1]=='':
+									moves.append(coord1)
+									count -= 1
+							elif (direction == DRI):
+								if self.board[coord1.row-1][coord1.col-1]=='':
+									moves.append(coord1)
+									count -= 1
+								elif self.board[coord1.row+1][coord1.col+1]=='':
+									moves.append(coord1)
+									count -= 1
+							else:
+								if self.board[coord1.row-1][coord1.col+1]=='':
+									moves.append(coord1)
+									count -= 1
+								elif self.board[coord1.row+1][coord1.col-1]=='':
+									moves.append(coord1)
+									count -= 1
+						if (count > 0):
+							moves.append(self.get_legal_move(count))
+
+		if returnMoves == 1:
+			return moves[0]
+		elif returnMoves == 2:
+			return moves[0],moves[1]
+
 		
 
-
-		return False
-
-	def get_legal_move(self):
-		freecoord = None
+	def get_legal_move(self,count):
+		freecoord = []
 		should_i_break_or_should_i_go=False
 		for r in range(len(self.board)):
 			for c in range(len(self.board[0])):
 				if self.board[r][c]=='':
-					freecoord=Coord(r,c)
-					should_i_break_or_should_i_go=True
-					break
+					freecoord.append(Coord(r,c))
+					if (len(freecoord)==count):
+						should_i_break_or_should_i_go=True
+						break
 			if should_i_break_or_should_i_go:
 				break
-		return freecoord
+		if count==1:
+			return freecoord[0]
+		elif count==2:
+			return freecoord[0],freecoord[1]
 
 	
 	def getCoord(self,tuple):
-		return Coord(tuble[0],tuple[1])
+		return Coord(tuple[0],tuple[1])
 
 	def put_enemy_stones(self,coord1,coord2=None):
 		self.board.put_token(coord1,self.enemy)
 		if coord2 != None:
 			self.board.put_token(coord2,self.enemy)
 	
+
+	def get_rows(self):
+		self.get_horizontal_rows()
+		self.get_vertical_rows()
 
 	def get_horizontal_rows(self):
 		
@@ -289,33 +503,7 @@ class Player:
 	def get_player_rows(self):
 		return sorted(self.off_rows, key=lambda number:(number[0], len(number[1])),reverse=True)
 	
-if __name__ == "__main__":
-	
-	player = Player()
-	player.get_next_move("L0000")
 
-	player.get_horizontal_rows()
-	player.get_vertical_rows()
-
-	player.get_enemy_rows()
-
-	player.board.print_board()
-
-	print "Enemy"
-	for row in player.get_enemy_rows():
-		print row
-
-	print "Me"
-	for row in player.get_player_rows():
-		print row
-
-	print player.get_alive_off(2)
-	
-
-	
-
-
-	
 
 		
 
