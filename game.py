e196ec4ed3a9c67614b325aba6cef7108e52a6d4
Denniz <d_zim@gmx.de>
2012-01-20 20:27:00 +0100

    ref

---
+++ game.py
@@ -0,0 +1,40 @@
+"""
+"	Authors: 	Nicolas Neu & Dennis Zimmermann
+"	Date:		20. January 2012
+"
+"	Rule based KI for Connect6
+"
+"	Open Issues:
+"		- Diagonal Rows
+"		- Error in referee 
+"""
+
+from referee import Referee
+from player import Player
+from dummyai import DummyAI
+
+
+class Game:
+
+	
+	def run(self):
+		ref = Referee()
+		p1 = Player()
+		p2 = DummyAI()
+
+		ref.add_player(p1)
+		ref.add_player(p2)
+
+		
+
+if __name__ == "__main__":
+		
+	g = Game()
+	g.run()
+
+		
+
+
+
+				
+	
